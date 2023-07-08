3a6907a1352afa3a485a3782a386ebae844d6a7b
Prateek <prateek.kumar.original@gmail.com>
2023-07-08 20:54:39 +0530

    Sign Up feature added

--- index.js
+++ index.js
@@ -3,22 +3,55 @@ const https = require('https');
 const bodyParser = require('body-parser');
 const app = express()
 
-app.use(bodyParser.urlencoded({extended:true}))
+//API key - d7bc4d3eaa9f28d956fd89498896b911-us13
+//List/audience id - bd2075e240
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname+"/signup.html")
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
 })
 
-app.post("/", (req, res)=>{
-    fname = req.body.fname
-    lname = req.body.lname
-    email = req.body.email
-    console.log(fname);
-    console.log(lname);
-    console.log(email);
-})
+app.post("/", (req, res) => {
+    const fname = req.body.fname
+    const lname = req.body.lname
+    const email = req.body.email
 
-app.listen(process.env.PORT || 3000, ()=>{
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname,
+                    // BIRTHDAY: 
+                }
+
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const list_id = "bd2075e240"
+    const url = `https://us13.api.mailchimp.com/3.0/lists/${list_id}`
+    const options = {
+        method: 'POST',
+        auth: "anystring:d7bc4d3eaa9f28d956fd89498896b911-us13"
+    };
+    const request = https.request(url, options, function (response) {
+        response.on("data", (data)=>{
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on http://127.0.0.1:3000/");
 })
